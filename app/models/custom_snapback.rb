1dfa09fc5b3013ca800fb79d9506225e6ead990e
ankita dangi <ankita.dangi@zingbytes.com>
2018-01-09 17:54:22 +0530

    chnages in add to text

--- app/models/custom_snapback.rb
+++ app/models/custom_snapback.rb
@@ -3,6 +3,7 @@ class CustomSnapback < ApplicationRecord
 	mount_uploader :back_image, BackImageUploader
 	mount_uploader :left_image, LeftImageUploader
 	mount_uploader :right_image, RightImageUploader
+	mount_uploader :logo, LogoUploader
 	belongs_to :order, optional: true
 	has_many :order_items
 end
